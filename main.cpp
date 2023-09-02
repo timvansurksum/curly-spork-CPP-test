59b3ef43365d70d78c3465b5a642492bcbfa4282
Tim <targa2002260@gmail.com>
2023-09-02 21:50:05 +0200

    initial tests with loops my brain melts :/

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::string first_names[5] = {
+            "Tim",
+            "Sara",
+            "Oliver",
+            "Rachel",
+            "Chloe"
+    };
+    std::string Last_names[5] = {
+                "van Surksum",
+                "Lance",
+                "Queen",
+                "Zane",
+                "Decker"
+    };
+    int max_index = 5;
+    for (int i = 0; max_index > i;) {
+        for (int n = 0; n < max_index - i;) {
+            std::cout  << "\t";
+            n++;
+        }
+        std::cout << "hello " << first_names[max_index-i] << " " << Last_names[max_index-i] << " how are you?\n";
+        i++;
+    }
 }
