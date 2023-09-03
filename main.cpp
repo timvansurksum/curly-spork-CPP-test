a790ace2d6633122d3e888ab154e41540bb26e55
Tim <targa2002260@gmail.com>
2023-09-03 18:51:30 +0200

    fixed loops and made list size automatic

--- main.cpp
+++ main.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 
 int main() {
-    std::string first_names[5] = {
+    //    arrays of first and last names
+    std::string first_names[] = {
             "Tim",
             "Sara",
             "Oliver",
             "Rachel",
             "Chloe"
     };
-    std::string Last_names[5] = {
+    std::string Last_names[] = {
                 "van Surksum",
                 "Lance",
                 "Queen",
                 "Zane",
                 "Decker"
     };
-    int max_index = 5;
+    // gets size of names lists
+    int max_index = sizeof(Last_names) / sizeof(std::string);
+    // prints each name out
     for (int i = 0; max_index > i;) {
+        i++;
         for (int n = 0; n < max_index - i;) {
             std::cout  << "\t";
             n++;
         }
         std::cout << "hello " << first_names[max_index-i] << " " << Last_names[max_index-i] << " how are you?\n";
-        i++;
     }
 }
