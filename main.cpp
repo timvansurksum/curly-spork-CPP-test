f19954c7eb3b7aa12581d585feef262352378ffb
Tim <targa2002260@gmail.com>
2023-09-04 07:15:23 +0200

    startet work on numerical integration

--- main.cpp
+++ main.cpp
@@ -1,4 +1,18 @@
 #include <iostream>
+#include<cmath>
+double f(double x) {
+    return log(pow(sin(x), 2));
+}
+
+double integrate(double a, double b, double delta_x_input){
+    double area = 0;
+    double N = (b - a) / delta_x_input
+    double delta_x = (b-a)/N;
+    for(int n = 0; n > N; n++){
+        area += f(a+n*delta_x)*delta_x;
+    }
+    return area;
+}
 
 int main() {
     //    arrays of first and last names
@@ -27,4 +41,5 @@ int main() {
         }
         std::cout << "hello " << first_names[max_index-i] << " " << Last_names[max_index-i] << " how are you?\n";
     }
+
 }
