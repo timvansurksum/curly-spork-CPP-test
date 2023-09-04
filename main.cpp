c6d8e67b8b7a1e9cc69e92c49c00a186d527e600
Tim <targa2002260@gmail.com>
2023-09-04 08:45:31 +0200

    finished work on numerical integration

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include<cmath>
+
 double f(double x) {
-    return log(pow(sin(x), 2));
+    return exp(-pow(x, 2));
 }
 
 double integrate(double a, double b, double delta_x_input){
@@ -16,32 +17,18 @@ double integrate(double a, double b, double delta_x_input){
 }
 
 int main() {
-    std::cout << "the integral of log(pow(sin(x), 2)) between 1 and 2 is: " << integrate(1, 2, 0.000001);
-    //    arrays of first and last names
-    std::string first_names[] = {
-            "Tim",
-            "Sara",
-            "Oliver",
-            "Rachel",
-            "Chloe"
-    };
-    std::string Last_names[] = {
-                "van Surksum",
-                "Lance",
-                "Queen",
-                "Zane",
-                "Decker"
-    };
-    // gets size of names lists
-    int max_index = sizeof(Last_names) / sizeof(std::string);
-    // prints each name out
-    for (int i = 0; max_index > i;) {
-        i++;
-        for (int n = 0; n < max_index - i;) {
-            std::cout  << "\t";
-            n++;
-        }
-//        std::cout << "hello " << first_names[max_index-i] << " " << Last_names[max_index-i] << " how are you?\n";
-    }
-
+    double a;
+    std::cout << "please enter your lower bound: ";
+    std::cin >> a;
+    double b;
+    std::cout << "please enter your upper bound: ";
+    std::cin >> b;
+    double delta_x_input;
+    std::cout << "please enter your delta x: ";
+    std::cin >> delta_x_input;
+    double area = integrate(a, b, delta_x_input);
+    std::cout << "the integral of log(pow(sin(x), 2))"
+    << "\nbetween " << a << "\nand " << b
+    << "\nwith a delta x of " << delta_x_input
+    << "\nis approximately: " << area;
 }
